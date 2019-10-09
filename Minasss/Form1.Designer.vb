35d3c2212509a59d7123736de5818eb9eb5a6bbc
Gonçalo Gomes 11º4 <Gonalo Gomes 114@WIN-GESFLHLR4EE>
2019-10-09 12:17:00 +0100

    push das minas

--- Minasss/Form1.Designer.vb
+++ Minasss/Form1.Designer.vb
@@ -29,7 +29,7 @@ Partial Class Form1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(30, 22)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(178, 25)
         Me.Label1.TabIndex = 0
