5e538bad206e65f74c1ca3afbea7593455f924f6
Gonçalo Gomes 11º4 <Gonalo Gomes 114@WIN-GESFLHLR4EE>
2019-10-10 10:29:32 +0100

    Final

--- Minasss/Form1.Designer.vb
+++ Minasss/Form1.Designer.vb
@@ -28,10 +28,10 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(266, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(178, 25)
+        Me.Label1.Size = New System.Drawing.Size(195, 35)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Jogo das Minas"
         '
